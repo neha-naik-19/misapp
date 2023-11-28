3471318cf85b70a9c45d6e5a686d1cb309a099c1
neha-naik-19 <nehanaik1983@gmail.com>
2023-11-28 12:23:08 +0530

    28112023 commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var async = require("async");
 var moment = require("moment");
 var date = require("date-and-time");
 
-const port = 3001;
+const port = 3002;
 
 const { getAuthToken, appendData } = require("./googleSheetsService.js");
 
@@ -201,14 +201,108 @@ router.route("/add-faculty-preference").post((req, res) => {
 });
 
 //allot course to faculty and update in Google Sheet
+// router.route("/allot-course1").post((req, res) => {
+//   try {
+//     pool.getConnection((err, connection) => {
+//       const query = `
+//         SELECT *
+//         FROM facultypreference
+//         ORDER BY course_code, preference
+//       `;
+
+//       connection.query(query, async (err, results) => {
+//         if (err) {
+//           console.error("Error querying the database:", err);
+//           res.status(500).json({
+//             error: "Data could not be fetched from table" + err.sqlMessage,
+//           });
+//         }
+
+//         // Step 2: Initialize variables to track allocated courses and roles
+//         const allocatedFaculty = new Set();
+//         const allocatedRoles = new Set();
+
+//         // Step 3: Iterate through the sorted preferences and allocate courses
+//         const finalwrite = [];
+//         const auth = await getAuthToken();
+//         results.forEach((preferences) => {
+//           //courseid, authorid, facultyrole, totalnoofcsstudnets, totalnootherdisciplinestudents, comments, semid
+//           const {
+//             course_code,
+//             faculty_id,
+//             facultyrole,
+//             totalnoofcsstudnets,
+//             totalnootherdisciplinestudents,
+//             comments,
+//             semid,
+//             preference,
+//           } = preferences;
+
+//           // Check if the role is already allocated for this course
+//           if (
+//             !allocatedRoles.has(`${course_code}-${facultyrole}`) &&
+//             !allocatedFaculty.has(`${faculty_id}-${facultyrole}`)
+//           ) {
+//             // Allocate the course to the faculty
+//             console.log(
+//               `Allocating ${course_code} to ${faculty_id} with role ${facultyrole}`
+//             );
+//             allocatedFaculty.add(`${faculty_id}-${facultyrole}`);
+//             allocatedRoles.add(`${course_code}-${facultyrole}`);
+//             //courseid, authorid, facultyrole, totalnoofcsstudnets, totalnootherdisciplinestudents, comments, semid
+//             // const data = [
+//             //   faculty_id,
+//             //   course_code,
+//             //   facultyrole,
+//             //   totalnoofcsstudnets,
+//             //   totalnootherdisciplinestudents,
+//             // ];
+
+//             const data = [
+//               course_code,
+//               faculty_id,
+//               facultyrole,
+//               totalnoofcsstudnets,
+//               totalnootherdisciplinestudents,
+//               comments,
+//               semid,
+//               // preference,
+//             ];
+//             finalwrite.push(data);
+//           }
+//         });
+//         appendData(auth, finalwrite);
+//         res.status(201).json({
+//           message: "Course allocation done",
+//         });
+//       });
+//     });
+//   } catch (error) {
+//     console.error("Error:", error);
+//     res.status(500).json({
+//       error: "Failed to create sample ALLOC statement",
+//     });
+//   }
+// });
+
 router.route("/allot-course").post((req, res) => {
   try {
     pool.getConnection((err, connection) => {
-      const query = `
-        SELECT *
-        FROM facultypreference
-        ORDER BY course_code, preference
-      `;
+      // const query = `
+      //   SELECT *
+      //   FROM facultypreference
+      //   ORDER BY course_code, preference
+      // `;
+
+      // const query = `
+      //   SELECT *
+      //   FROM coursepreference
+      //   ORDER BY courseid, preferences
+      // `;
+
+      const query = `SELECT courseid, authorid, facultyrole, totalnoofcsstudnets, 
+                     totalnootherdisciplinestudents, comments, (SELECT semid FROM sem) as semid 
+                     FROM coursepreference ORDER BY courseid, preferences`;
 
       connection.query(query, async (err, results) => {
         if (err) {
@@ -222,37 +316,70 @@ router.route("/allot-course").post((req, res) => {
         const allocatedFaculty = new Set();
         const allocatedRoles = new Set();
 
+        // console.log('results :- ',results);
+
         // Step 3: Iterate through the sorted preferences and allocate courses
         const finalwrite = [];
         const auth = await getAuthToken();
         results.forEach((preferences) => {
+          // const {
+          //   faculty_id,
+          //   course_code,
+          //   preference,
+          //   facultyrole,
+          //   totalnoofcsstudnets,
+          //   totalnootherdisciplinestudents,
+          // } = preferences;
+
+          // const {
+          //   authorid,
+          //   courseid,
+          //   Preferences,
+          //   facultyrole,
+          //   totalnoofcsstudnets,
+          //   totalnootherdisciplinestudents,
+          // } = preferences;
+
           const {
-            faculty_id,
-            course_code,
-            preference,
+            courseid,
+            authorid,
             facultyrole,
             totalnoofcsstudnets,
             totalnootherdisciplinestudents,
+            comments,
+            semid,
+            preference,
           } = preferences;
 
           // Check if the role is already allocated for this course
           if (
-            !allocatedRoles.has(`${course_code}-${facultyrole}`) &&
-            !allocatedFaculty.has(`${faculty_id}-${facultyrole}`)
+            !allocatedRoles.has(`${courseid}-${facultyrole}`) &&
+            !allocatedFaculty.has(`${authorid}-${facultyrole}`)
           ) {
             // Allocate the course to the faculty
             console.log(
-              `Allocating ${course_code} to ${faculty_id} with role ${facultyrole}`
+              `Allocating ${courseid} to ${authorid} with role ${facultyrole}`
             );
-            allocatedFaculty.add(`${faculty_id}-${facultyrole}`);
-            allocatedRoles.add(`${course_code}-${facultyrole}`);
+            allocatedFaculty.add(`${authorid}-${facultyrole}`);
+            allocatedRoles.add(`${courseid}-${facultyrole}`);
+            // const data = [
+            //   authorid,
+            //   courseid,
+            //   facultyrole,
+            //   totalnoofcsstudnets,
+            //   totalnootherdisciplinestudents,
+            // ];
+
             const data = [
-              faculty_id,
-              course_code,
+              courseid,
+              authorid,
               facultyrole,
               totalnoofcsstudnets,
               totalnootherdisciplinestudents,
+              comments,
+              semid,
             ];
+
             finalwrite.push(data);
           }
         });
@@ -278,21 +405,21 @@ router.route("/allot-ta-allocation").post((req, res) => {
                           tacou left join course cou ON tacou.coursepreference1 = cou.courseid
                           GROUP BY tacou.coursepreference1 ORDER BY cnt;`;
 
-  const coursepreference2 = `select tacou.coursepreference2, COUNT(tacou.coursepreference2) cnt, cat from tacoursepreference 
+  const coursepreference2 = `select tacou.coursepreference2, COUNT(tacou.coursepreference2) cnt from tacoursepreference 
   tacou left join course cou ON tacou.coursepreference2 = cou.courseid
-  GROUP BY tacou.coursepreference2 ORDER BY cat, cnt ;`;
+  GROUP BY tacou.coursepreference2 ORDER BY cnt;`;
 
-  const coursepreference3 = `select tacou.coursepreference3, COUNT(tacou.coursepreference3) cnt, cat from tacoursepreference 
+  const coursepreference3 = `select tacou.coursepreference3, COUNT(tacou.coursepreference3) cnt from tacoursepreference 
   tacou left join course cou ON tacou.coursepreference3 = cou.courseid
-  GROUP BY tacou.coursepreference3 ORDER BY cat, cnt ;`;
+  GROUP BY tacou.coursepreference3 ORDER BY cnt;`;
 
-  const coursepreference4 = `select tacou.coursepreference4, COUNT(tacou.coursepreference4) cnt, cat from tacoursepreference 
+  const coursepreference4 = `select tacou.coursepreference4, COUNT(tacou.coursepreference4) cnt from tacoursepreference 
   tacou left join course cou ON tacou.coursepreference4 = cou.courseid
-  GROUP BY tacou.coursepreference4 ORDER BY cat, cnt ;`;
+  GROUP BY tacou.coursepreference4 ORDER BY cnt;`;
 
-  const coursepreference5 = `select tacou.coursepreference5, COUNT(tacou.coursepreference5) cnt, cat from tacoursepreference 
+  const coursepreference5 = `select tacou.coursepreference5, COUNT(tacou.coursepreference5) cnt from tacoursepreference 
   tacou left join course cou ON tacou.coursepreference5 = cou.courseid
-  GROUP BY tacou.coursepreference5 ORDER BY cat, cnt ;`;
+  GROUP BY tacou.coursepreference5 ORDER BY cnt;`;
 
   // const studentDetails = `select student_id, name,cat, recordtimestamp, coursepreference1, coursepreference2, coursepreference3,
   //                           coursepreference4, coursepreference5 from tacoursepreference WHERE status = 1 ORDER by cat, recordtimestamp,
@@ -378,60 +505,76 @@ router.route("/allot-ta-allocation").post((req, res) => {
       getPhdPreferenceStudents.forEach((data) => {
         pref1.push(
           return_data.coursepreference1.filter(
-            (elem) => elem.coursepreference1 === data.coursepreference1
+            (elem) => data.coursepreference1 === elem.coursepreference1
           )
         );
       });
+
+      // const pref1 = getPhdPreferenceStudents.map((data) => {
+      //   const filtered = return_data.coursepreference1.filter(
+      //     (elem) => data.coursepreference1 === elem.coursepreference1
+      //   );
+      //   return {
+      //     data: data.coursepreference1,
+      //     filteredData: filtered,
+      //     cnt: filtered.length, // Element count
+      //   };
+      // });
+
+      // Sort pref1 based on cnt (element count) in ascending order
+      // pref1.sort((a, b) => a.cnt - b.cnt);
 
       let result = [];
       let i = 0;
 
       return_data.studentDetails.forEach((data) => {
+        //student_id, email, name, cgpa, cat, courseid, status, semid
         let {
           recordtimestamp,
-          cat,
           student_id,
+          email,
           name,
+          cgpa,
+          cat,
           coursepreference1,
           cname1,
+          statuss,
+          semid,
           alloted,
         } = data;
 
         for (let index = 0; index < getPhdPreferenceStudents.length; index++) {
           if (student_id === getPhdPreferenceStudents[index]["student_id"]) {
-            if (alloted === undefined)
-              alloted = `${cname1} (${coursepreference1})`;
+            if (alloted === undefined) alloted = coursepreference1;
             break;
           }
         }
 
         for (let index = 0; index < getHdPreferenceStudents.length; index++) {
           if (student_id === getHdPreferenceStudents[index]["student_id"]) {
-            if (alloted === undefined)
-              alloted = `${cname1} (${coursepreference1})`;
+            if (alloted === undefined) alloted = coursepreference1;
             break;
           }
         }
 
         for (let index = 0; index < getFdPreferenceStudents.length; index++) {
           if (student_id === getFdPreferenceStudents[index]["student_id"]) {
-            if (alloted === undefined)
-              alloted = `${cname1} (${coursepreference1})`;
+            if (alloted === undefined) alloted = coursepreference1;
             break;
           }
         }
 
-        result.push({
-          recordtimestamp,
-          cat,
-          student_id,
-          name,
-          coursepreference1,
-          cname1,
-          alloted,
-        });
+        // result.push({
+        //   recordtimestamp,
+        //   cat,
+        //   student_id,
+        //   name,
+        //   coursepreference1,
+        //   cname1,
+        //   alloted,
+        // });
 
-        console.log(result);
+        // console.log(result);
 
         result.forEach((data) => {
           for (let index = 0; index < sql.length; index++) {
@@ -448,14 +591,27 @@ router.route("/allot-ta-allocation").post((req, res) => {
 
         // alloted = "test";
 
+        //student_id, email, name, cgpa, cat, courseid, status, semid
+        // const data1 = [
+        //   moment(recordtimestamp).format("YYYY/MM/DD HH:mm:ss"),
+        //   student_id,
+        //   cat,
+        //   name,
+        //   alloted,
+        // ];
+
         const data1 = [
-          moment(recordtimestamp).format("YYYY/MM/DD HH:mm:ss"),
-          cat,
           student_id,
+          email,
           name,
+          cgpa,
+          cat,
           alloted,
+          // cname1,
+          statuss,
+          semid,
         ];
-        // finalwrite.push(data1);
+        finalwrite.push(data1);
       });
 
       // console.log(typeof return_data.studentDetails);
@@ -466,15 +622,16 @@ router.route("/allot-ta-allocation").post((req, res) => {
       //   const { student_id, name, cat, recordtimestamp } = data;
 
       //   const data1 = [student_id, name, cat, recordtimestamp];
-      //   // finalwrite.push(data1);
+      // finalwrite.push(data1);
       // });
 
       appendDataTa(auth, finalwrite);
       res.status(201).json({
         // message: "TA allocation done",
         // message: return_data.studentDetails,
-        phd: return_data.sql,
-        // pref: pref1,
+        // phd: return_data.coursepreference1,
+        pref: return_data.studentDetails,
+        // 1: return_data.coursepreference1,
       });
 
       // res.send(return_data);
